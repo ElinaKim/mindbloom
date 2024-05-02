b2942a3ca089de6a36f2c3470615ef370c27cd0b
Elina Kim <elinakim791@gmail.com>
2024-05-01 20:02:28 -0700

    initial login form with validation

--- src/routes/login.lazy.tsx
+++ src/routes/login.lazy.tsx
@@ -1,5 +1,47 @@
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { CustomInput } from '../components/input'
 import { createLazyFileRoute } from '@tanstack/react-router'
 
 export const Route = createLazyFileRoute('/login')({
-  component: () => <div>Hello /login!</div>
+  component: LoginComponent,
 })
+
+type LoginComponentInputs = {
+  userName: string
+  password: string
+}
+
+function LoginComponent() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginComponentInputs>()
+
+  const onSubmit: SubmitHandler<LoginComponentInputs> = (data) => alert(JSON.stringify(data))
+
+  return (
+    <>
+      <form 
+        className='w-[80%]'
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <CustomInput 
+          labelText='User Name'
+          id='username'
+          errorMessage={errors.userName ? 'Invalid User Name' : undefined}
+          aria-invalid={errors.userName ? "true" : "false"}
+          {...register('userName', { required: true })}
+        />
+        <CustomInput 
+          labelText='Password'
+          id='password'
+          errorMessage={errors.password ? 'Invalid Password' : undefined}
+          aria-invalid={errors.password ? "true" : "false"}
+          {...register('password', { required: true })}
+        />
+        <button type='submit'> Submit </button>
+      </form>
+    </>
+  )
+}
