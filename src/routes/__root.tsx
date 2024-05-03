51f8714277363ede91fa0decefd78695540d2f33
Elina Kim <elinakim791@gmail.com>
2024-05-03 00:11:11 -0700

    update navbar style

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,18 +5,18 @@ import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 export const Route = createRootRoute({
   component: () => (
     <>
-      <nav className='flex justify-between items-center m-4'>
+      <nav className='flex justify-between items-center m-4 md:mx-8 md:my-4'>
         <div className='flex items-center'>
             <img className='w-10' src={Logo} alt='site logo' />
             <h2 className='font-poppins font-extrabold text-xl'>mindbloom</h2>
         </div>
         <div>
           <ul className='flex gap-2.5'>
-              <Link to="/login" className="[&.active]:font-bold">
+              <Link to="/login" className="px-2 rounded [&.active]:bg-light-grey hover:bg-light-grey">
                 Login
               </Link>
               <div className='border border-grey'></div>
-              <Link to="/register" className="[&.active]:font-bold">
+              <Link to="/register" className="px-2 rounded [&.active]:bg-light-grey hover:bg-light-grey">
                 Register
               </Link>
           </ul>
