4c4c9e0a71f711875025df44abbd50569f94011b
Elina Kim <40552613+ElinaKim@users.noreply.github.com>
2024-05-05 19:04:05 -0700

    Feature/setup server (#3)
    * move gitignore to root
    
    * setup database with knex

---
+++ server/seed-data/user.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+      id: 1,
+      user_name: "Ella",
+      email: "ellasmith@gmail.com",
+      password: "goodpassword"
+    },
+    {
+      id: 2,
+      user_name: "Mark",
+      email: "eayscough1@pagesperso-orange.fr",
+      password: "verysmartpassword123"
+    },
+  ];
