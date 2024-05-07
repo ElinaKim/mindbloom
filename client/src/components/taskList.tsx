df7b21608c3d8dda1d2a74fb446481d7241f0b39
Elina Kim <elinakim791@gmail.com>
2024-05-07 01:53:57 -0700

    handle deleting the task

--- client/src/components/taskList.tsx
+++ client/src/components/taskList.tsx
@@ -1,27 +1,37 @@
 import { TaskCheckbox } from "./taskCheckbox"
 import DeleteIcon from '../assets/icons/delete.svg'
 import type { Task } from '../types/task'
-
-const handleDelete = () => { console.log('delete') }
+import * as tasksApi from '../api/taskApi'
+import { useState } from 'react';
 
 export interface TaskListProps {
   tasks: Task[]
 }
 
-export function TaskList({ tasks }: TaskListProps) {
-  const handleClick = () => {
-    handleDelete();
-  }
+export function TaskList({ tasks: initialTasks }: TaskListProps) {
+  const [tasks, setTasks] = useState<Task[]>(initialTasks)
+
+  const handleClick = async (task_id: number) => {
+    try {
+      await tasksApi.deleteTask({ task_id });
+      const updatedTasks = tasks.filter(task => task.id !== task_id);
+      setTasks(updatedTasks);
+      console.log('task deleted successfuly')
+    } catch (error) {
+      console.error('Error deleting task:', error);
+    }
+  };
+
   return (
     <>
       {
         tasks.map(task => {
           return (
-            <div key={task.id} className='flex items-center bg-white border-light-grey border-2 pt-4 p-2 rounded hover:border-pink hover:border-2 shadow'>
+            <div key={task.id} className='flex items-center bg-white border-light-grey border-2 pt-4 p-2 rounded hover:border-blue hover:border-2 shadow'>
               <TaskCheckbox />
               <p className='place-self-center p-4 w-[100%]'>{task.task_name}</p>
               <button
-                onClick={handleClick}
+                onClick={() => handleClick(task.id)}
                 className='self-end'
               >
                 <img
