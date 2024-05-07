364ea8a31d1d971d0648f7655c8f1a46d0723868
Elina Kim <elinakim791@gmail.com>
2024-05-06 22:50:31 -0700

    create task types

---
+++ client/src/types/task.ts
@@ -0,0 +1,10 @@
+export interface Task {
+  id: number
+  user_id: number
+  task_name: string
+  description: string
+  due_date: string
+  tag_name: string
+  created_at: string
+  updated_at: string
+}
