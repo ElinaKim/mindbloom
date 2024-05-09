73150a5f46e5e9de50d14fd0fe729c2fa1a40177
Elina Kim <elinakim791@gmail.com>
2024-05-08 20:10:34 -0700

    implement edit form

--- client/src/routes/_auth.tasks.upcoming.tsx
+++ client/src/routes/_auth.tasks.upcoming.tsx
@@ -6,6 +6,7 @@ import { TaskMenu } from '../components/taskMenu';
 import { TaskList } from '../components/taskList';
 import { TaskForm } from '../components/taskForm';
 import type { Task } from '../types/task';
+import { EditTaskForm } from '../components/editTaskForm';
 
 export const Route = createFileRoute('/_auth/tasks/upcoming')({
   component: UpcomingTasksComponent,
@@ -13,6 +14,7 @@ export const Route = createFileRoute('/_auth/tasks/upcoming')({
 
 function UpcomingTasksComponent() {
   const [user, setUser] = useState<any>(null)
+  const [selectedTaskId, setSelectedTaskId] = useState<number | null>(null)
   const [loading, setLoading] = useState(false)
   const [tasks, setTasks] = useState<Task[]>([])
   const [showModal, setShowModal] = useState(false)
@@ -40,6 +42,12 @@ function UpcomingTasksComponent() {
     }
   }
 
+  const handleTaskClick = (taskId: number) => {
+    if (taskId && Number.isInteger(taskId) && taskId > 0) {
+      setSelectedTaskId(taskId)
+    }
+  }
+
   if (!user || loading) {
     return <div>Loading upcoming tasks</div>
   }
@@ -48,37 +56,51 @@ function UpcomingTasksComponent() {
     setShowModal(true)
   }
 
-  const handleFormSubmit = () => {
-    preload() // trigger re-render by fetching updated tasks
-  }
-
   return (
     <>
       <div className='flex content-center h-screen bg-taskBackground bg-no-repeat bg-cover'>
         <div className='flex-shrink-0'>
           <TaskMenu />
         </div>
-        <div className='w-[80vw] flex flex-col justify-between m-auto mt-2 md:mt-4 p-4 rounded'>
+        <div className='w-[80vw] flex flex-col justify-between m-auto mt-2 md:mt-8 p-4'>
           <h1 className='text-xl md:text-4xl'>Hello, {user.user_name}</h1>
           <h2 className='text-xl mt-2 pb-2 md:text-2xl md:mt-4 font-kumbh-bold'>Tasks</h2>
           <h2 className='text-base mt-2 pb-2 md:text-xl md:mt-4 font-kumbh-bold'>Upcoming</h2>
-          <button className='self-start my-4 py-2 w-[25%] rounded-md text-white bg-black' onClick={openModal}>
+          <button className='self-start my-4 py-2 w-full md:w-[25%] rounded-md text-white bg-black' onClick={openModal}>
             Add New Task
           </button>
           <div className=''>
             <div className='md:mr-4'>
-              {loading ? <p>Tasks Loading...</p> : <TaskList tasks={tasks} />}
+              {
+                loading
+                  ? <p>Tasks Loading...</p>
+                  : <TaskList tasks={tasks} onTaskClick={handleTaskClick} />
+              }
             </div>
           </div>
         </div>
       </div>
-      {showModal && (
+      {
+        showModal &&
         <div className="fixed justify-center items-center top-0 left-0 w-[100%] h-full bg-black bg-opacity-60">
           <div className="">
-            <TaskForm closeModal={() => setShowModal(false)} onFormSubmit={handleFormSubmit} />
+            <TaskForm
+              closeModal={() => setShowModal(false)}
+              onFormSubmit={preload}
+            />
           </div>
         </div>
-      )}
+      }
+      {
+        selectedTaskId &&
+        <div className="fixed justify-center items-center top-0 left-0 w-full h-full bg-black bg-opacity-60">
+          <EditTaskForm
+            closeModal={() => setSelectedTaskId(null)}
+            onFormSubmit={preload}
+            taskId={selectedTaskId}
+          />
+        </div>
+      }
     </>
   )
 }
